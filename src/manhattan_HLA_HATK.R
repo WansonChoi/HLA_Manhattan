0d200b2ffca9df3d4af755b4e928e92af83d3562
Wanson Choi <wschoi.bhlab@gmail.com>
2019-04-08 17:03:33 +0900

    (manhattan_HLA_HATK.R) 7 to 2 ratio is proper to the plot and buttom part.

--- src/manhattan_HLA_HATK.R
+++ src/manhattan_HLA_HATK.R
@@ -4,6 +4,7 @@ args = commandArgs(trailingOnly = TRUE)
 
 p_assoc.logsitic_ = strsplit(args[1], ',')[[1]]
 plot_label_ = args[2]
+plot_label_ = ""
 out_ = args[3]
 
 pointcol_ = args[4]
@@ -36,10 +37,11 @@ source(file.path(p_src_, "assocplot_HATK.R"))
 
 #### Main
 
-pdf(paste0(out_, ".pdf"), width=10, height=7, pointsize=10) # implementation test (2018.4.25)
+pdf(paste0(out_, ".pdf"), width=16, height=9, pointsize=10) # implementation test (2018.4.25)
 
 NumberofLogistic = length(p_assoc.logsitic_)
-layout(matrix(1:(NumberofLogistic+1), (NumberofLogistic+1),1, byrow=TRUE), heights=c(rep(5, NumberofLogistic),3))
+layout(matrix(1:(NumberofLogistic+1), (NumberofLogistic+1),1, byrow=TRUE), heights=c(rep(7, NumberofLogistic),2))
+# (2019. 04. 01.) One manhattan graph : One fancy buttom = 7 : 2
 
 # (2018. 8. 10.) No more `r.data` for colorRamp().
 
@@ -51,7 +53,7 @@ for (i in 1:NumberofLogistic) {
 
   locus <- TRIM_LOGISTIC.ASSOC(p_assoc.logsitic_[i])
   # manhattan plot
-  make.fancy.locus.plot.bare("6", (if(i == 1) plot_label_ else ""), locus, min.pos_, max.pos_, max.yaxis_[i], topsignal_[i], NULL, seq(0,max.yaxis_[i],by=5), pointcolor=pointcol_, topcolor=topcol_)
+  make.fancy.locus.plot.bare("6", (if(i == 1) plot_label_ else ""), locus, min.pos_, max.pos_, max.yaxis_[i], topsignal_[i], NULL, seq(0,max.yaxis_[i],by=10), topcolor=topcol_)
   
 }
 # genomic position belt.
