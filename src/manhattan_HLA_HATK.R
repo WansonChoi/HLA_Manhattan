dfc935a12f2e986952f1cbe46662d973c3dce28d
Wanson Choi <wschoi.bhlab@gmail.com>
2019-04-09 12:54:48 +0900

    (manhattan_HLA_HATK.R) No more 'plot_label_' argument. 'pointsize_' and 'yaxis_unit_' arguments are newly added.

--- src/manhattan_HLA_HATK.R
+++ src/manhattan_HLA_HATK.R
@@ -3,23 +3,24 @@ args = commandArgs(trailingOnly = TRUE)
 
 
 p_assoc.logsitic_ = strsplit(args[1], ',')[[1]]
-plot_label_ = args[2]
-plot_label_ = ""
-out_ = args[3]
+# plot_label_ = args[]
+out_ = args[2]
 
-pointcol_ = args[4]
+pointcol_ = args[3]
+pointsize_ = as.numeric(args[4])
 topcol_ = args[5]
 min.pos_ = as.numeric(args[6])
 max.pos_ = as.numeric(args[7])
 topsignal_ = strsplit(args[8], ',')[[1]] # label of top signal marker
 max.yaxis_ = as.numeric(strsplit(args[9], ',')[[1]])
+yaxis_unit_ = as.numeric(args[10])
 
-p_GeneBuild_ = args[10]
-p_src_ = args[11]
+p_GeneBuild_ = args[11]
+p_src_ = args[12]
 
 ### Checking manually.
 
-# for (i in 1:10) {
+# for (i in 1:13) {
 #   print(args[i])
 # }
 
@@ -30,6 +31,7 @@ p_src_ = args[11]
 # print(max.yaxis_)
 # print(length(max.yaxis_))
 
+print(args)
 
 ########## <Loading Necessary Scripts(Modules)> ##########
 source(file.path(p_src_, "assocplot_HATK.R"))
@@ -37,7 +39,7 @@ source(file.path(p_src_, "assocplot_HATK.R"))
 
 #### Main
 
-pdf(paste0(out_, ".pdf"), width=16, height=9, pointsize=10) # implementation test (2018.4.25)
+pdf(paste0(out_, ".pdf"), width=16, height=9, pointsize=pointsize_) # `pointsize` is now manipulated by the argument 'pointsize_'. (2019. 04. 09.)
 
 NumberofLogistic = length(p_assoc.logsitic_)
 layout(matrix(1:(NumberofLogistic+1), (NumberofLogistic+1),1, byrow=TRUE), heights=c(rep(7, NumberofLogistic),2))
@@ -53,7 +55,7 @@ for (i in 1:NumberofLogistic) {
 
   locus <- TRIM_LOGISTIC.ASSOC(p_assoc.logsitic_[i])
   # manhattan plot
-  make.fancy.locus.plot.bare("6", (if(i == 1) plot_label_ else ""), locus, min.pos_, max.pos_, max.yaxis_[i], topsignal_[i], NULL, seq(0,max.yaxis_[i],by=10), topcolor=topcol_)
+  make.fancy.locus.plot.bare("6", (if(i == 1) "" else ""), locus, min.pos_, max.pos_, max.yaxis_[i], topsignal_[i], NULL, seq(0,max.yaxis_[i],by=yaxis_unit_), topcolor=topcol_)
   
 }
 # genomic position belt.
